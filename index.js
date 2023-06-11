1874ed11e1fefdf5a769aa23d32e9fc89490dba5
pbullock08 <126013657+pbullock08@users.noreply.github.com>
2023-06-11 17:16:36 -0400

    started working on functions to view tables and add values into the database based on user input

--- index.js
+++ index.js
@@ -4,51 +4,183 @@ const mysql = require('mysql2');
 // import inquirer
 const inquirer = require('inquirer');
 
-// import tabel 
-const table = require('table');
+// import console.table
+const cTable = require('console.table');
 
-inquirer
-    .prompt([
-        {
-            type: 'list',
-            message: 'What would you like to do?',
-            name: 'choice',
-            choices: [
-                {
-                    name: 'View All Employees',
-                    value: 'view_employees'
-                },
-                {
-                    name: 'Add Employee',
-                    value: 'add_employee'
-                },
-                {
-                    name: 'Update Employee Role',
-                    value: 'update_role'
-                },
-                {
-                    name: 'View All Roles',
-                    value: 'view_roles'
-                },
-                {
-                    name: 'Add Role',
-                    value: 'add_role'
-                },
-                {
-                    name: 'View Departments',
-                    value: 'view_departments'
-                },
-                {
-                    name: 'Add Department',
-                    value: 'add_department'
-                },
-                {
-                    name: 'Quit',
-                    value: 'quit'
-                },
-            ]
-        }
-    ])
-    .then((selection) => {
-        console.log(selection.choice);
-    });
+// import figlet for banner
+const figlet = require('figlet');
+
+// connect to database 
+const db = mysql.createConnection(
+    {
+        host: '127.0.0.1',
+        user: 'root',
+        password: 'rootroot',
+        database: 'organization_db'
+    },
+    console.log(`Connected to the organization_db database.`)
+)
+
+//show banner
+figlet("Employee Manager", function (err, data) {
+    if (err) {
+        console.log("Something went wrong...");
+        console.dir(err);
+        return;
+    }
+    console.log(data);
+    showPrompts();
+});
+
+// function to get user input on what they'd like to do
+function showPrompts() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                message: 'What would you like to do?',
+                name: 'choice',
+                choices: [
+                    {
+                        name: 'View All Employees',
+                        value: 'view_employees'
+                    },
+                    {
+                        name: 'Add Employee',
+                        value: 'add_employee'
+                    },
+                    {
+                        name: 'Update Employee Role',
+                        value: 'update_role'
+                    },
+                    {
+                        name: 'View All Roles',
+                        value: 'view_roles'
+                    },
+                    {
+                        name: 'Add Role',
+                        value: 'add_role'
+                    },
+                    {
+                        name: 'View Departments',
+                        value: 'view_departments'
+                    },
+                    {
+                        name: 'Add Department',
+                        value: 'add_department'
+                    },
+                    {
+                        name: 'Quit',
+                        value: 'quit'
+                    },
+                ]
+            }
+        ])
+        .then((selection) => {
+            // console.log(selection.choice);
+            switch (selection.choice) {
+                case 'view_employees':
+                    viewEmployees();
+                    break;
+                case 'add_employee':
+                    addEmployee();
+                    break;
+                case 'view_roles':
+                    viewRoles();
+                    break;
+                case 'view_departments':
+                    viewDepartments();
+                    break;
+                case 'add_department':
+                    addDepartment();
+                    break;
+                default:
+                    process.exit();
+                    break;
+            }
+        });
+};
+
+// view employees 
+function viewEmployees() {
+    db.query('SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(e.first_name," ", e.last_name) AS manager FROM employee JOIN role ON role.id = employee.role_id JOIN department ON department.id = role.department_id LEFT JOIN employee e on employee.manager_id = e.id;', function (err, results) {
+        console.table(results)
+        showPrompts()
+    })
+};
+
+// add employee
+function addEmployee() {
+    
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is the employees first name?',
+                name: 'employee_fn'
+            },
+            {
+                type: 'input',
+                message: 'What is the employees last name?',
+                name: 'employee_ln'
+            },
+            {
+                type: 'list',
+                message: 'What is the employees role id?',
+                name: 'employee_role',
+                choices: [1, 2, 3, 4, 5, 6, 7, 8]
+            },
+            {
+                type: 'list',
+                message: 'What is their managers id?',
+                name: 'employee_mgr',
+                choices: [1, 2, 3, 4, 5, 6, 7, 8]
+            },
+        ])
+        .then((e) => {
+            if (e.employee_mgr === 'None') {
+                db.query('INSERT INTO employee (first_name, last_name, ) ')
+            }
+            console.log(e);
+        })
+};
+
+// view roles
+function viewRoles() {
+    db.query('SELECT role.id, role.title, department.name AS department, role.salary FROM role JOIN department ON department.id = role.department_id;', function (err, results) {
+        console.table(results)
+        showPrompts()
+    })
+};
+
+// view all departments
+function viewDepartments() {
+    db.query('SELECT * FROM department;', function (err, results) {
+        console.table(results)
+        showPrompts()
+    })
+};
+
+// add department
+function addDepartment() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is the name of the department?',
+                name: 'name'
+            }
+        ])
+        .then((d) => {
+            // console.log(d.name);
+            db.query(`INSERT INTO department (name) VALUES ("${d.name}")`, function (err, results) {
+            })
+        })
+        .then((results) => {
+            db.query('SELECT * FROM department;', function (err, results) {
+                console.table(results)
+                showPrompts()
+            })
+        })
+};
+
